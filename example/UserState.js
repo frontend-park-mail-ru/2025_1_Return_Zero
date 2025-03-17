5be2584e12ec9c9f388abad99dc6c3930e9d5bba
zeritonik <zaq31337@gmail.com>
2025-03-18 02:37:32 +0300

    Добавлен пример типа инсты с 1ой лекции. В сам фреймворк добавлено автоматическое очищение колбеков со стейтов при удалении компонента из дома. Пофикшен баг с выбором базового элемента для компонента, css-класс компонента переведён в нижний регистр. Добавлена ДОФИГИЩА логов.

---
+++ example/UserState.js
@@ -0,0 +1,8 @@
+import { State } from "../rzf/State.js";
+
+
+export const userState = new class UserState extends State {
+    constructor() {
+        super(undefined);
+    }
+}(); // сделал так чтобы логи были с именем класса
--- rzf/State.js
+++ rzf/State.js
@@ -7,14 +7,28 @@ export class State {
         this.callbacks = [];
     }
 
-    onChange(callback) {
+    addCallback(callback) {
         this.callbacks.push(callback);
+
+        console.log(`Added callback [${callback}] to ${this.constructor.name}, total: [${this.callbacks}]`);
+    }
+
+    removeCallbacks(callbacks) {
+        this.callbacks = this.callbacks.filter(cb => !callbacks.includes(cb));
+
+        console.log(`Removed [${callbacks}] callbacks from ${this.constructor.name}, left: [${this.callbacks}]`);
     }
 
     setState(value) {
+        console.log(`Set state from ${this.value} to ${value}`);
+
         const prev = this.value;
         this.value = value;
-        this.callbacks.slice().reverse().forEach(callback => callback(prev, this.value));
+
+        this.callbacks.slice().reverse().forEach(callback => {
+            console.log(`Calling callback "${callback}" for ${this.constructor.name}`);
+            callback(prev, this.value)
+        });
     }
 
     getState() {
