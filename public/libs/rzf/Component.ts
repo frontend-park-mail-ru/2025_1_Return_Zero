cc4dee746fc91454a2e7f32fa14c884e4378f0a8
zeritonik <zaq31337@gmail.com>
2025-05-26 15:37:18 +0300

    вернул таймаут на setState

--- public/libs/rzf/Component.ts
+++ public/libs/rzf/Component.ts
@@ -21,7 +21,7 @@ export abstract class Component {
     setState(state: Record<string, any>) {
         this.state = { ...this.state, ...state };
         const tempComponentVNode = {...this.vnode!};
-        update(this.vnode, tempComponentVNode);
+        queueMicrotask(() => update(this.vnode!, tempComponentVNode));
     }
 
     abstract render(): VNode[];
